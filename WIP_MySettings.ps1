6d54218694a1bcee688b63c3fa64a43795fe9761
Nuno Brito <brito.n@outlook.pt>
2023-08-04 18:33:27 +0100

    updating

---
+++ WIP_MySettings.ps1
@@ -0,0 +1,22 @@
+# Global vars
+$backup_datetoday = Get-Date -Format FileDateTime
+$backup_user = echo ($env:USERNAME)
+
+$backup_destination_path = "C:\Users\$backup_user\Desktop\SettingsBackup-$backupdatetoday"
+
+$wt_config_path = "C:\Users\$backup_user\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+$vsc_config_path = "C:\Users\$backup_user\AppData\Roaming\Code\User"
+$pwsh_profile_path = "C:\Users\$backup_user\Documents\PowerShell"
+
+# Windows Terminal
+xcopy $wt_config_path\settings.json
+
+# Oh-my-posh
+winget install JanDeDobbeleer.OhMyPosh -s winget
+New-Item -Path $PROFILE -Type File -Force
+oh-my-posh init pwsh | Invoke-Expression
+# Themes:
+# slim
+# atomic
+# blueish
+# jandedobbeleer
