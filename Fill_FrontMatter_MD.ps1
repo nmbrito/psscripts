96bceac885122d77300c57e8f787f50e4543630b
Nuno Brito <65853815+nmbrito@users.noreply.github.com>
2024-07-13 08:57:14 +0100

    Update Fill_FrontMatter_MD.ps1

--- Fill_FrontMatter_MD.ps1
+++ Fill_FrontMatter_MD.ps1
@@ -13,14 +13,14 @@ Concluído:
 Estado:
 Plataformas:
 "@
-if ($($_.Steam) -ne "") 			{Add-Content -Path .\gen\$($_.Title).md -Value "  - Steam"				}
-if ($($_.GOG) -ne "") 				{Add-Content -Path .\gen\$($_.Title).md -Value "  - GOG"				}
-if ($($_.MicrosoftStore) -ne "")	{Add-Content -Path .\gen\$($_.Title).md -Value "  - Microsoft Store"	}
-if ($($_.UbisoftConnect) -ne "") 	{Add-Content -Path .\gen\$($_.Title).md -Value "  - Ubisoft Connect"	}
-if ($($_.EA) -ne "") 				{Add-Content -Path .\gen\$($_.Title).md -Value "  - EA"					}
-if ($($_.Battlenet) -ne "") 		{Add-Content -Path .\gen\$($_.Title).md -Value "  - Battle.net"			}
-if ($($_.EpicGamesStore) -ne "") 	{Add-Content -Path .\gen\$($_.Title).md -Value "  - Epic Games Store"	}
-if ($($_.Nintendo) -ne "") 			{Add-Content -Path .\gen\$($_.Title).md -Value "  - Nintendo"			}
+if ($($_.Steam) -ne "")             {Add-Content -Path .\gen\$($_.Title).md -Value "  - Steam"               }
+if ($($_.GOG) -ne "")               {Add-Content -Path .\gen\$($_.Title).md -Value "  - GOG"                 }
+if ($($_.MicrosoftStore) -ne "")    {Add-Content -Path .\gen\$($_.Title).md -Value "  - Microsoft Store"     }
+if ($($_.UbisoftConnect) -ne "")    {Add-Content -Path .\gen\$($_.Title).md -Value "  - Ubisoft Connect"     }
+if ($($_.EA) -ne "")                {Add-Content -Path .\gen\$($_.Title).md -Value "  - EA"                  }
+if ($($_.Battlenet) -ne "")         {Add-Content -Path .\gen\$($_.Title).md -Value "  - Battle.net"          }
+if ($($_.EpicGamesStore) -ne "")    {Add-Content -Path .\gen\$($_.Title).md -Value "  - Epic Games Store"    }
+if ($($_.Nintendo) -ne "")          {Add-Content -Path .\gen\$($_.Title).md -Value "  - Nintendo"            }
 Add-Content -Path .\gen\$($_.Title).md -Value @"
 Proezas: 
 Comentários: 
